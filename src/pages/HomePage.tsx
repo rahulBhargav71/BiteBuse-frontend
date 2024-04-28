d560a55e34968c60a694f672f44bedd8d58c15eb
rahulBharagv71 <rahul.20225071@mnnit.ac.in>
2024-04-28 10:40:57 +0530

    search feature updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const HomePage = () => {
   const navigate=useNavigate();
   const handleSearchSubmit=(searchFormValues:SearchForm)=>{
